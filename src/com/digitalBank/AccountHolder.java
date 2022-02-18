a143d77558f048d9b5144c5121e9403fcd27e039
Yan Oliveira <oliveira.yan02@gmail.com>
2022-02-18 14:52:15 -0300

    feat: add account holder class

---
+++ src/com/digitalBank/AccountHolder.java
@@ -0,0 +1,13 @@
+package com.digitalBank;
+
+public class AccountHolder {
+	private String name;	
+	
+	public AccountHolder(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
